8f05d2e30c698ff43d18f60328a9da88a2d6b966
Samarjeet <samarjeetbanik@gmail.com>
2021-10-01 09:11:24 +0530

    updated Hello.java

--- Hello.java
+++ Hello.java
@@ -4,7 +4,7 @@ public class Hello
 {
     public static void main(String [] args)
     {
-    	if (System.out.printf("Welocome all to the Hacktoberfest-2021") == null) {
+    	if (System.out.printf("Welcome all to the Hacktoberfest-2021") == null) {
         }
     }
 }
